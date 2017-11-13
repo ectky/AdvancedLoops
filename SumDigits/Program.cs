53255e2f629203ffb720e3a48e8e795471289d31
Elitsa <ectky3@gmail.com>
2017-11-13 16:16:11 +0200

    Add files via upload

---
+++ SumDigits/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumDigits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            var sum = n % 10;
+            n /= 10;
+            while (n != 0)
+            {
+                sum += n % 10;
+                n /= 10;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
